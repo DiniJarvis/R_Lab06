94e592432ac941d2c144d001eb60c028b1cfdb97
DiniJarvis <dinesh.sundar2006@gmail.com>
2022-10-06 11:13:47 +0200

    first commit

---
+++ lab6pack/R/knapsack_dynamic.R
@@ -0,0 +1,52 @@
+#' Knapsack problem using greedy algortihm
+#'
+#' @param x a data.frame with two variables v and w (values and weights)
+#' @param W size (Max weight capacity)
+#'
+#' @return the maximum values and elements to be carried by knapsack
+#' @examples knapsack_dynamic(x = knapsack_objects[1:8,], W = 3500)
+#' @examples knapsack_dynamic(x = knapsack_objects[1:12,], W = 2000)
+#' @export
+#'
+
+knapsack.dynamic <-function(x,W){
+  stopifnot(is.data.frame(x),
+            is.numeric(W),
+            W>0,
+            is.numeric(x$w),
+            is.numeric(x$v),
+            x$v>=0,
+            x$w>=0)
+  
+  mat<- matrix(nrow = 1+nrow(x), ncol = 1+W)
+  mat[,1]<-0
+  mat[1,]<-0
+  for (i in 2:nrow(mat)){
+    for (j in 2:ncol(mat)){
+      if (x$w[i-1]>(j-1)){
+        mat[i,j]<-mat[i-1,j]
+      } else if (x$w[i-1] <= j-1){
+        mat[i,j]=max(mat[i-1,j],x$v[i-1]+mat[i-1,j-x$w[i-1]])
+      }
+    }
+  }
+  
+  elements=c()
+  k=nrow(x)+1
+  l=W+1
+  while (k>1 & l>1) {
+    if(mat[k,l] != mat[k-1,l]){
+      elements<-c(k-1,elements)
+      k<-k-1
+      l<-l-1
+    }else{
+      k<-k-1
+      
+    }
+  }
+  output <- list(
+    value = round(mat[nrow(mat),ncol(mat)]),
+    elements = elements
+  )
+  return(output)
+}
